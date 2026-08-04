a173385e25a09e26e6906c6b2dd6fa822dc24c23
Mathnerd20 <mpcc.akshay@gmail.com>
2026-08-04 12:08:24 +0530

    Add Q_1_summary.java with basic Java operations

---
+++ lab2/Q_1_summary.java
@@ -0,0 +1,33 @@
+package lab2;
+
+import java.util.Scanner;
+import java.math.*;
+
+public class Q_1_summary {
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int x = 6;
+		String coffee = "It's \"coffee\" time";
+		if(++x > 4 && x++ <=10) System.out.println("Message: " + coffee);
+		System.out.println(x);
+		String hello = "hello";
+		System.out.println(hello + " " + hello.length());
+		System.out.println(hello.toUpperCase());
+		String phrase = "Hello this is akshay";
+		System.out.println("Index of Akshay: " + phrase.indexOf("akshay"));
+		System.out.println("Character at 3th position of hello: " + hello.charAt(3));
+		hello += " and " + x;
+		System.out.println(hello + " ");
+		x = (int)Math.pow(2, hello.length());
+		System.out.println(x);
+		String result = (x > 0) ? "X is positive" : "X is negative or zero";  
+		System.out.println(result);
+		String[] bool = {"Yes", "No"};
+		for(String i: bool)
+		{
+			System.out.println(i);
+		}
+		input.close();
+	}
+}
