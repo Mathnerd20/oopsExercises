865dc9836079cc2de764ecd4754fe009477393c1
Mathnerd20 <mpcc.akshay@gmail.com>
2026-08-11 10:51:19 +0530

    Create Q_N_surface.java

---
+++ lab3/Q_N_surface.java
@@ -0,0 +1,29 @@
+package E3_25MCCE28;
+
+import java.util.Scanner;
+
+class Box{
+	int length = 1;
+	int width = 1;
+	int height = 1;
+	Box(int l, int b, int h)
+	{
+		length = l;
+		width = b;
+		height = h;
+	}
+	public int getSA()
+	{
+		return 2*(length*width + width*height + height*length);
+	}
+}
+
+public class Q_N_surface {
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter dimensions of box(l,b,h): ");
+		Box a = new Box(input.nextInt(),input.nextInt(), input.nextInt());
+		System.out.println("The total surface area is: " + a.getSA());
+		input.close();
+	}
